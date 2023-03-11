babf85e23cb779ebd777d8902c55ed16a2d57606
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-03-11 12:54:41 +0300

    github project link  has been created

---
+++ src/components/GithubBadge.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import IconGithub from "./IconGithub.vue";
+</script>
+
+<template>
+  <a href="https://github.com/mustafadalga/mailto-link-generator"
+     className="absolute bg-black/75 w-20 h-20 -top-10 -right-10 rotate-45 z-20">
+
+    <div className="absolute left-1/2 -translate-x-1/2 bottom-2">
+      <IconGithub/>
+    </div>
+  </a>
+</template>
