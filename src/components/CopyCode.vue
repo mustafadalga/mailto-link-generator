f01968a6726e209c60b6fc240fa54c286f16159e
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-03-11 12:55:53 +0300

    generated code copy component has been created

--- src/components/CopyCode.vue
+++ src/components/CopyCode.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import type { PropType } from "vue";
-import type { IForm } from "@/types";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 const props = defineProps({
-  form: {
-    type: Object as PropType<IForm>,
+  title: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
     required: true
   }
 })
@@ -13,12 +15,6 @@ const props = defineProps({
 let timer: number | undefined;
 const showAnimate = ref(false);
 
-const mailTo = computed<string>(() => {
-
-  const template: string = `mailto:${props.form.to}?cc=${props.form.cc}&bcc${props.form.bcc}&subject=${props.form.subject}&body=${props.form.body}`;
-  return template;
-
-});
 
 function copyCode() {
   if (timer) {
@@ -28,25 +24,29 @@ function copyCode() {
   }
 
   showAnimate.value = true;
-  navigator.clipboard.writeText(mailTo.value);
+  navigator.clipboard.writeText(props.code);
   timer = window.setTimeout(() => showAnimate.value = false, 500);
 }
 </script>
 
 <template>
-  <div
-      class="fixed bottom-6 inset-x-1/2	-translate-x-1/2 -translate-y-1/2 w-[calc(100%-3rem)] max-w-3xl flex items-center justify-between gap-3 px-4 py-2.5 rounded-full	bg-black/75">
+    <div class="grid min-w-0">
+      <label class="pl-5 text-sm text-gray-600">{{ title }}</label>
+      <div
+          class="flex items-center justify-between gap-3 px-4 py-2.5 rounded-full	bg-black/75 min-w-0">
 
-    <p class="w-full text-white whitespace-nowrap overflow-hidden text-ellipsis text-sm font-medium">
-      {{ mailTo }}
-    </p>
 
-    <button
-        @click="copyCode"
-        class="whitespace-nowrap bg-blue-500 rounded-full text-white text-sm lg:text-base px-2 py-1 w-32">
+        <p class="w-full text-white whitespace-nowrap overflow-hidden text-ellipsis text-sm font-medium">
+          {{ code }}
+        </p>
 
-      <span v-if="showAnimate">Copied!</span>
-      <span v-else>Copy Code</span>
-    </button>
-  </div>
+        <button
+            @click="copyCode"
+            class="whitespace-nowrap bg-blue-500 rounded-full text-white text-sm lg:text-base px-2 py-1 w-32">
+
+          <span v-if="showAnimate">Copied!</span>
+          <span v-else>Copy Code</span>
+        </button>
+      </div>
+    </div>
 </template>
